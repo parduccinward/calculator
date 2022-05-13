38b0aff1fd0c1cd0c17a24895e391405648866d6
Diego <dpardoblacutt@gmail.com>
2022-05-13 10:44:45 -0400

    Add operator function that depending on the operator run a function

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(x,y){
 function divide(x,y){
     return x/y;
 }
+
+function operate(operator,x,y){
+    switch(operator){
+        case "+":
+            add(x,y);
+            break;
+        case "-":
+            subtract(x,y);
+            break;
+        case "*":
+            multiply(x,y);
+            break;
+        case "/":
+            divide(x,y);
+            break;
+        default:
+            throw new Error("Invalid operator");
+    }
+}
