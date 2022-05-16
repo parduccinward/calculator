5d45dac2ea0ebd5d033327306bd5e88a738eb436
Diego <dpardoblacutt@gmail.com>
2022-05-16 13:02:07 -0400

    Refactor code to distinct between make numbers and operators clickable, and display them

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let displayText = "";
-makeNumbersClickable();
+makeButtonsClickable();
 
 function add(x,y){
     return x+y;
@@ -32,20 +32,63 @@ function operate(operator,x,y){
     }
 }
 
+function makeButtonsClickable(){
+    makeNumbersClickable();
+    makeOperatorsClickable();
+}
+
 function makeNumbersClickable(){
     const buttons = document.querySelectorAll(".digit-btn");
     buttons.forEach(button => {
-        button.addEventListener("click",updateDisplay)
+        button.addEventListener("click",addNewNumber)
     })
 }
 
-function updateDisplay(e){
-    text = saveDisplayText(displayText,e.target.id);
-    displayTextElement = document.querySelector(".expression-text");
+function makeOperatorsClickable(){
+    const buttons = document.querySelectorAll(".operator-btn");
+    buttons.forEach(button => {
+        button.addEventListener("click",addNewOperator);
+    })
+}
+
+function addNewNumber(e){
+    let number = e.target.id;
+    addNewElementToDisplay(number);
+}
+
+function addNewOperator(e){
+    let operator = e.target.id;
+    checkOperatorNumber(operator);
+}
+
+function addNewElementToDisplay(string){
+    let text = appendNewDisplayText(displayText,string);
+    addToDisplay(text);
+}
+
+function addToDisplay(text){
+    let displayTextElement = getDisplayText();
     displayTextElement.textContent = text;
 }
 
-function saveDisplayText(oldText, newText){
+function appendNewDisplayText(oldText, newText){
     displayText = oldText + newText
     return displayText;
+}
+
+function getDisplayText(){
+    return document.querySelector(".expression-text");
+}
+
+function checkOperatorNumber(operator){
+    let operatorNumber =1;
+    if(operatorNumber==0){
+        addNewElementToDisplay(operator);
+    }else{
+        console.log("Operating first expression"); //change every console with a func
+        const result = 19;
+        console.log("the result is " + result);
+        console.log("removing old display text");
+        console.log("the new display text is : " + result+" "+ operator);
+    }
 }
